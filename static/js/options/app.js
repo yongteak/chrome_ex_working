64521b9e9d15708f7c02fbf8a5bd5821b0c5f9c8
yongtaek <skillove@gmail.com>
2020-12-04 02:34:06 +0900

    update controllers

--- static/js/options/app.js
+++ static/js/options/app.js
@@ -2,35 +2,58 @@ var app = angular.module('app', ["ngRoute",
 	'app.controllers', 'app.services'
 ]);
 
-app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+app.constant('CONFIG', {
+	'STORAGE_TABS' : 'tabs',
+	'STORAGE_BLACK_LIST' : 'black_list',
+	'STORAGE_RESTRICTION_LIST' : 'restriction_list',
+	'STORAGE_NOTIFICATION_LIST' : 'notification_list',
+	'STORAGE_NOTIFICATION_MESSAGE' : 'notification_message',
+	'STORAGE_TIMEINTERVAL_LIST' : 'time_interval'
+})
+
+app.config(["$routeProvider", "$locationProvider", /*"$compileProvider", */
+($routeProvider, /*$compileProvider,*/ $locationProvider) => {
+	// aHrefSanitizationTrustedUrlList
+	// $compileProvider.aHrefSanitizationTrustedUrlList(/^\s*(https?|local|data|chrome-extension):/);
+	// $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|local|data|chrome-extension):/);
+
 	$routeProvider.
 		when("/v1/setting", {
-			templateUrl: "tmpl/options/main.html"
+			templateUrl: "tmpl/options/setting.html",
+			controller: 'settingController'
 		}).
 		when("/v1/limit", {
-			templateUrl: "tmpl/options/limit.html"
+			templateUrl: "tmpl/options/limit.html",
+			controller: 'limitController'
 		}).
 		when("/v1/alarm", {
-			templateUrl: "tmpl/options/alarm.html"
+			templateUrl: "tmpl/options/alarm.html",
+			controller: 'alarmController'
 		}).
 		when("/v1/data", {
-			templateUrl: "tmpl/options/data.html"
+			templateUrl: "tmpl/options/data.html",
+			controller: 'dataController'
 		}).
 		when("/v1/sync", {
-			templateUrl: "tmpl/options/sync.html"
+			templateUrl: "tmpl/options/sync.html",
+			controller: 'syncController'
 		}).
 		when("/v1/status", {
-			templateUrl: "tmpl/options/status.html"
+			templateUrl: "tmpl/options/status.html",
+			controller: 'statusController'
 		}).
 		when("/v1/profile", {
-			templateUrl: "tmpl/options/profile.html"
+			templateUrl: "tmpl/options/profile.html",
+			controller: 'profileController'
 		}).
 		when("/v1/about", {
-			templateUrl: "tmpl/options/about.html"
+			templateUrl: "tmpl/options/about.html",
+			controller: 'aboutController'
 		}).
 		otherwise({
 			redirectTo: '/v1/setting'
 		});
+		
 	$locationProvider.hashPrefix('');
 }])
 
--- static/js/options/controllers.js
+++ static/js/options/controllers.js
@@ -1,10 +1,38 @@
 // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/%EC%95%A0%EB%A1%9C%EC%9A%B0_%ED%8E%91%EC%85%98
+// function를 생락하고 '=>' 기호 사용하지 말것
+// https://stackoverflow.com/questions/47696945/over-function-throwing-me-an-angular-error
+
 angular.module('app.controllers', [])
-    .controller('view', function ($scope) {
+    .controller('side', ($scope, $location) => {
+        $scope.isCurrentPath = path => {
+            return $location.path().indexOf(path) != -1;
+        };
+    })
+    .controller('view', $scope => {
         // $scope.count = 0;
         console.log("view!");
 
         $scope.init = () => {
             console.log("view > init!");
         }
-    });
+    })
+    .controller('settingController', function ($scope, $location,$filter, identity,storage,CONFIG) {
+        // console.log(CONFIG);
+        // storage.getValue(CONFIG.STORAGE_TABS, e => {
+        //     // console.log(e)
+        // });
+    })
+    .controller('limitController', function ($scope, $location) {
+    })
+    .controller('alarmController', function ($scope, $location) {
+    })
+    .controller('dataController', function ($scope, $location) {
+    })
+    .controller('syncController', function ($scope, $location) {
+    })
+    .controller('statusController', function ($scope, $location) {
+    })
+    .controller('profileController', function ($scope, $location) {
+    })
+    .controller('aboutController', function ($scope, $location) {
+    })
