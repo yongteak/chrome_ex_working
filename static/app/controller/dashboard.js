9f1c26751c31844ceeb50fb493c7c92b08bd6bbc
yongteak <skillove@gmail.com>
2021-01-07 15:19:10 +0900

    dashboard working

--- static/app/controller/dashboard.js
+++ static/app/controller/dashboard.js
@@ -2,7 +2,8 @@ angular.module('app.controller.dashboard', [])
     .controller('dashboardController', function ($scope, $rootScope, $filter, $timeout, $http, moment, storage, CONFIG) {
         $scope.model = {
             charts: {
-                traffic: {}
+                traffic: {},
+                scatter: {}
             }
         }
 
@@ -13,22 +14,31 @@ angular.module('app.controller.dashboard', [])
             $scope.model.charts.limit = { 'option': chart(), 'click': null };
 
             $scope.model.charts.monthly = { 'option': chart2(), 'click': null };
+            $scope.model.charts.scatter = { 'option': chart3(), 'click': null };
         }, 0.2 * 1000);
+
+        function percent(count, times, usaged) {
+            var sum = count + times + usaged;
+            return [Math.round(count / sum * 100),
+            Math.round(count / sum * 100),
+            Math.round(count / sum * 100)];
+        };
 
         function chart(key, series) {
             return {
                 xAxis: {
                     type: 'category',
                     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                    show: false
+                    show: true
                 },
                 yAxis: {
                     type: 'value',
                     show: false,
-                    min: 800
+                    // min: 800
                 },
                 tooltip: {
                     trigger: 'axis',
+                    formatter: series => { return '' },
                     axisPointer: {
                         type: 'cross',
                         crossStyle: {
@@ -42,10 +52,10 @@ angular.module('app.controller.dashboard', [])
                     data: [' ', ' ']
                 },
                 grid: {
-                    left: 8,
+                    left: 10,
                     top: 4,
-                    right: 5,
-                    bottom: 2
+                    right: 6,
+                    bottom: 18
                 },
                 series: [{
                     data: Array.from({ length: 7 }, () => Math.floor(Math.random() * 500)),
@@ -62,7 +72,7 @@ angular.module('app.controller.dashboard', [])
 
         function chart2(key, series) {
             return {
-                xAxis:{
+                xAxis: {
                     type: 'category',
                     data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
                 },
@@ -72,15 +82,25 @@ angular.module('app.controller.dashboard', [])
                     data: [' ', ' ']
                 },
                 grid: {
-                    left: 50,// 데이터 최대값 계측 평균 값 구현 필요
-                    top: 18,
+                    left: 50,
+                    top: 6,
                     right: 5,
                     bottom: 20
                 },
-                yAxis:{
+                yAxis: {
                     type: 'value',
                     axisLabel: {
-                        formatter: value=> $filter('moneyFormat')(value)
+                        formatter: value => $filter('moneyFormat')(value)
+                    }
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    formatter: series => { return '' },
+                    axisPointer: {
+                        type: 'cross',
+                        crossStyle: {
+                            color: '#999'
+                        }
                     }
                 },
                 series: [{
@@ -89,6 +109,67 @@ angular.module('app.controller.dashboard', [])
                         barBorderRadius: 4
                     },
                     data: Array.from({ length: 12 }, () => Math.floor(Math.random() * 90000000))
+                }]
+            }
+        }
+
+        function chart3(key, series) {
+            return {
+                legend: {
+                    padding: 0,
+                    itemGap: 0,
+                    data: [' ', ' ']
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    formatter: series => { return '' },
+                    axisPointer: {
+                        type: 'cross',
+                        crossStyle: {
+                            color: '#999'
+                        }
+                    }
+                },
+                grid: {
+                    left: 50,
+                    top: 35,
+                    right: 12,
+                    bottom: 20
+                },
+                xAxis: {
+                    scale: true,
+                    max: 23,
+                    axisLabel: {
+                        formatter: '{value}h'
+                    }
+                },
+                yAxis: {
+                    show:false,
+                    scale: true,
+                    max: 3600,
+                    // minInterval: 60,
+                    axisLabel: {
+                        formatter: value => {
+                            console.log(value);
+                            value = value % 60 == 0 ? (value / 60) + 'm' : value;
+                            return value;
+                        }
+                    }
+                },
+                series:
+                [
+                    {
+                    type: 'effectScatter',
+                    symbolSize: 5,
+                    data: [
+                        [21, 1543],
+                        [12, 900]
+                    ]
+                },
+                 {
+                    type: 'scatter',
+                        data: Array.from({ length: 350 }, () =>
+                            [Math.floor(Math.random() * 25), Math.floor(Math.random() * 3600)]),
                 }]
             }
         }
